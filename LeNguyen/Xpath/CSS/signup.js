7aea95c0528988c625910e35ad54be16aa0aa1b6
lenguyen <le.nguyen@enouvo.com>
2025-06-30 17:08:57 +0700

    Add exercise Xpath

---
+++ LeNguyen/Xpath/CSS/signup.js
@@ -0,0 +1,23 @@
+//https://ngoaingu24h.vn/
+
+//button[text()='Đăng ký']
+
+//h2[text()='Đăng ký']
+
+//h4[@class='dialog-sub-title' and contains(text(), '1. Thông tin')]
+
+//h4[@class='dialog-sub-title' and contains(text(), '2. Thông tin')]
+
+//input[@name='account']
+
+//input[@name='fullName']
+
+//input[@name='password']
+
+//input[@autocomplete="email"]
+
+//input[@autocomplete='phonenumber']
+
+//h4[@class='dialog-sub-title' and contains(text(), '2. Thông tin')]//parent::div/following-sibling::div//button[text()='Đăng ký']
+
+//span[@class='dialog-link']
